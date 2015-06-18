64d03f16781e62cdbeab89416dd5bcea4b773f4e
Mark Lybrand <mlybrand@gmail.com>
2015-06-17 20:58:45 -0700

    initial commit

---
+++ OfflineWebapi/Context/InventoryContext.cs
@@ -0,0 +1,14 @@
+﻿using OfflineWebapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OfflineWebapi.Context
+{
+    public class InventoryContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+    }
+}
---
+++ OfflineWebapi/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OfflineWebapi.Context;
+
+namespace OfflineWebapi.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private InventoryContext db = new InventoryContext();
+
+        public ActionResult Index()
+        {
+            var foo = db.Items.ToList();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
